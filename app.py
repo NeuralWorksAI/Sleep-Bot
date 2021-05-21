d002a12f3e2d95ee1f905b9fdcad4bd94a779880
00MB <s2061204@ed.ac.uk>
2021-05-21 19:08:25 +0100

    stable

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-client = discord.Client()
+intents = discord.Intents().all()
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+async def on_member_update(before, after):
+    print("test")
+    if before.status is discord.Status.offline and after.status is discord.Status.online:
+        print('was offline then online')
+        channel = client.get_channel(845354528819118121)  # notification channel
+        await channel.send(f'{after.name} is now {after.status}')
 
 client.run(os.getenv('TOKEN'))
