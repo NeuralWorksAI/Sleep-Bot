c778e3ac99fc1e27c183c0e21cc16f2884ddeb45
00MB <s2061204@ed.ac.uk>
2021-05-21 18:38:04 +0100

    stable bot init

---
+++ app.py
@@ -0,0 +1,20 @@
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
