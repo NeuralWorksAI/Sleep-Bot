33734d8931e0047a2e923ab134bb1f3dfab46e3a
00MB <s2061204@ed.ac.uk>
2021-05-21 19:28:13 +0100

    stable

--- app.py
+++ app.py
@@ -13,10 +13,9 @@ async def on_ready():
 
 @client.event
 async def on_member_update(before, after):
-    print("test")
-    if before.status is discord.Status.offline and after.status is discord.Status.online:
-        print('was offline then online')
-        channel = client.get_channel(845354528819118121)  # notification channel
+    if after.id == int(os.getenv('USERID')):
+        print('Michael is online')
+        channel = client.get_channel(os.getenv('CHANNELID'))  # notification channel
         await channel.send(f'{after.name} is now {after.status}')
 
 client.run(os.getenv('TOKEN'))
