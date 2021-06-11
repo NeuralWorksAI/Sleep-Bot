ccb6d7b5df72b2852529d5719726a441b1ff5e56
00MB <s2061204@ed.ac.uk>
2021-06-11 08:04:36 +0100

    moving to python bc nodejs sucks

--- commands/leaderboard.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "leaderboard",
-  description: "View the highest active sleep streaks",
-  guildOnly: true,
-  args: false,
-  execute(message, args) {
-    message.reply("Leaderboard: \nMichael: 9\nSarah: 4\nJames: 2");
-  },
-};
--- commands/viewstreak.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "mystreak",
-  description: "View your current sleep streak",
-  guildOnly: true,
-  args: false,
-  execute(message, args) {
-    message.reply("Your current streak is, 0");
-  },
-};
